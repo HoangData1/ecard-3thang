7799d63b399ccdc7a0a23b68178def070cdb4b8d
HoangData1 <136047758+HoangData1@users.noreply.github.com>
2025-07-14 21:46:09 +0700

    add

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,10 +5,10 @@ function Card() {
   const [showVideo, setShowVideo] = useState(false);
 
   const messages = [
-    'Ba tháng – không quá dài, nhưng đủ để biết rằng: Dù có những lúc không hiểu nhau, có lúc mệt mỏi đến mức muốn rời đi… Nhưng cuối cùng, cả hai vẫn chọn ở lại.',
-    'Cảm ơn em lúc nào cũng nghĩ cho anh, lúc nào cũng hỏi anh ổn không, nhìn vào mắt em là thấy em yêu anh nhiều như thế nào, anh cảm ơn em vì đã ở cạnh anh đã chọn anh là người đồng hành.',
-    'Anh biết em bé của anh nhạy cảm lắm, có những lúc anh vô ý mà làm em bùn anh biết chứ mà anh ngốc quá ha, nhưng mong em hãy tha thứ cho kẻ ngốc này nhé.',
-    'Anh mong mình sẽ cùng nhau vượt qua những khó khăn, để hiểu và thương nhau nhiều hơn nữa. — Từ anh, người luôn muốn giữ em lại bên đời.'
+    'Ba tháng – không dài, nhưng đủ để thấy: Dù có lúc mệt mỏi, cả hai vẫn chọn ở lại bên nhau.',
+    'Cảm ơn em luôn quan tâm, luôn hỏi anh ổn không. Nhìn vào mắt em, anh biết mình được yêu rất nhiều.',
+    'Anh biết em rất nhạy cảm, đôi khi anh vô tâm khiến em buồn. Tha thứ cho kẻ ngốc này nhé.',
+    'Anh mong ta sẽ cùng vượt qua mọi khó khăn, để hiểu và thương nhau nhiều hơn nữa.'
   ];
 
   useEffect(() => {
